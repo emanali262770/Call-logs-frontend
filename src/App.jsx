26dd341904a906f046232bef89f9ad0d9196f6dc
Ali Gates <hacktech877@gmail.com>
2025-09-08 16:52:04 +0500

    Code update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route, Outlet } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Login from "./pages/admin/Login";
 import Signup from "./pages/admin/Signup";
 import AdminLayout from "./pages/admin/AdminLayout";
@@ -16,6 +16,11 @@ import AddMeeting from "./pages/admin/AddMeeting";
 import Category from "./pages/admin/Category";
 import ProtectedRoute from "./components/ProtectedRoute";
 import MeetingFollowPage from "./components/Dashboard/followupmodel";
+import GroupManagement from "./pages/admin/GroupManagement";
+import Users from "./pages/admin/Users";
+import AssignRights from "./pages/admin/AccessControl";
+import Modules from "./pages/admin/Modules";
+import ModulesFunctionalities from "./pages/admin/ModulesFunctionalities";
 
 function AppContent() {
   return (
@@ -23,7 +28,7 @@ function AppContent() {
       <main className="flex-1">
         <Routes>
           {/* Public Routes */}
-          <Route path="/" element={<Login/>} />
+          <Route path="/" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
 
           {/* Admin Routes */}
@@ -33,22 +38,27 @@ function AppContent() {
               <ProtectedRoute>
                 <AdminLayout />
               </ProtectedRoute>
-               
             }
           >
             <Route index element={<AdminDashboard />} />
             <Route path="dashboard" element={<AdminDashboard />} />
             <Route path="staff" element={<StaffList />} />
             <Route path="products" element={<ProductsPage />} />
+            <Route path="products/add" element={<AddProduct />} />
             <Route path="customers" element={<CustomerData />} />
             <Route path="calendar" element={<Calendar />} />
             <Route path="followup" element={<FollowUp />} />
-            {/* new added meetingup page */}
             <Route path="meetingup" element={<MeetingFollowPage />} />
-            
-            <Route path="products/add" element={<AddProduct />} />
             <Route path="meetings/add" element={<AddMeeting />} />
             <Route path="category" element={<Category />} />
+
+            {/* Security */}
+            <Route path="groups" element={<GroupManagement />} />
+            <Route path="users" element={<Users />} />
+            
+            <Route path="access-rights" element={<AssignRights />} />
+            <Route path="modules" element={<Modules />} />
+            <Route path="modules-functionalities" element={<ModulesFunctionalities />} />
           </Route>
         </Routes>
       </main>
